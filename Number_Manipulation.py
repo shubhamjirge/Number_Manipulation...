b5ea60b29d73d86d645a71a61356211ae4ef954a
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2025-01-25 12:27:14 +0530

    Add files via upload

---
+++ Number_Manipulation.py
@@ -0,0 +1,27 @@
+# NUMBER MANIPULATION
+
+# Flooring a Number
+bmi = 84 / 1.65 ** 2
+print(int(bmi))
+
+# Rounding a Number
+print(round(bmi))
+print(round(bmi, 2))
+
+# Assignment Operators
+score = 0
+# user score a point
+score += 1
+print(score)
+
+# f-strings
+score = 0
+height = 1.83
+is_winning = True
+
+print(f"your score is: {score}, your height is {height}, your winning is {is_winning}")
+
+age = 12
+print(f"I am {age} years old")
+
+a = int("5") / int(2.7)
